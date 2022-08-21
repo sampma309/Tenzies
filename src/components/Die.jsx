c7ec4736c1f44e5496f5e57955b7cf0e5e231f51
Michael Samp <michael.samp94@gmail.com>
2022-08-20 22:52:38 -0500

    game now initializes with an empty board and generates new numbers on pressing start button. Once game begins, button changes function to roll

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,11 +1,16 @@
 import React from "react"
 
 export default function Die(props) {
+
+    const styles = {fontSize: props.gameState === 0 ? "0rem" : "3rem"}
+
     return (
         <h1 
             className={props.locked ? "dice locked" : "dice unlocked"}
             onClick={() => {
-                props.toggleLocked(props.id)}}>
+                props.toggleLocked(props.id)}}
+            style={styles}
+        >
             {props.value} {props.locked}
         </h1>
     )
--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -1,12 +1,23 @@
 import React from "react"
 
 export default function GameButton(props) {
+
+    let buttonText = "Start"
+    let buttonFunction = props.rollNewValues
+
+    if (props.gameState === 1) {
+        buttonText = "Roll"
+    } else if (props.gameState === 2) {
+        buttonText = "New Game"
+    }
+
     return (
         <div className="button--root">
             <button 
                 className="roll-button"
-                onClick={props.rollNewValues}>
-                Roll
+                onClick={props.gameState === 0 ? props.startGame : props.rollNewValues}
+            >
+                {buttonText}
             </button>
         </div>
 
